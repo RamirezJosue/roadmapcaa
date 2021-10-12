8c17046c99013bbbfccb1f08204cb5857f86dafa
RamirezJosue <xdemobtkx@gmail.com>
2021-10-11 22:16:54 -0500

    update

--- src/index.php
+++ src/index.php
@@ -25,7 +25,7 @@
         $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
 
         if (empty($tables)) {
-            echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
+            echo '<p class="center">No hay tablas en la base de datos </p>';
         } else {
             echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
             echo '<ul class="center">';

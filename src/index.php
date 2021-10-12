10485ad3e3fe4f6bd8e25b46535061d7558c5fd0
RamirezJosue <xdemobtkx@gmail.com>
2021-10-11 22:09:03 -0500

    update

--- src/index.php
+++ src/index.php
@@ -20,16 +20,20 @@
     <body>
         <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
         <?php
-            try {
-                $mbd = new PDO('mysql:host=mysql;dbname=demo', 'root', 'root');
-                foreach($mbd->query('SELECT * from prueba') as $fila) {
-                    print_r($fila);
-                }
-                $mbd = null;
-            } catch (PDOException $e) {
-                print "¡Error!: " . $e->getMessage() . "<br/>";
-                die();
+        $connection = new PDO('mysql:host=mysql;dbname=demo;charset=utf8', 'root', 'root');
+        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'demo'");
+        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
+
+        if (empty($tables)) {
+            echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
+        } else {
+            echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
+            echo '<ul class="center">';
+            foreach ($tables as $table) {
+                echo "<li>{$table}</li>";
             }
+            echo '</ul>';
+        }
         ?>
     </body>
 </html>
